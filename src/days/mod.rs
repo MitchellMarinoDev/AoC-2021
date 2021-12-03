318cf429f7ed38b34b830bb57330e8b3b147f5c2
mitchell <mitchoah@gmail.com>
2021-12-02 19:18:27 -0600

    Solved day 2

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -2,10 +2,14 @@ pub trait Day {
     fn solve() -> (String, String);
 }
 
-pub const DAYS: [fn() -> (String, String); 1] = [
+pub mod d1;
+pub mod d2;
+
+use d1::Day1;
+use d2::Day2;
+
+pub const DAYS: [fn() -> (String, String); 2] = [
     Day1::solve,
+    Day2::solve,
 ];
 
-pub mod d1;
-
-pub use d1::Day1;
