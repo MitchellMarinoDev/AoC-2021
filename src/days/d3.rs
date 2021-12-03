5d15ef4fc1a456787e437e7ac84085cc5da1b91d
Mitchell Marino <mmarino000@stu.waukesha.k12.wi.us>
2021-12-03 08:55:23 -0600

    worked on d3

---
+++ src/days/d3.rs
@@ -0,0 +1,82 @@
+use crate::days::Day;
+
+pub struct Day3;
+
+const INPUT: &str = "inputs/d3.txt";
+const BIT_WIDTH: u32 = 5;
+
+impl Day for Day3 {
+    fn solve() -> (String, String) {
+        let raw = std::fs::read_to_string(INPUT).expect("input file");
+        let report: Vec<_> = raw.lines()
+            .map(|l| u32::from_str_radix(l, 2).unwrap()).collect();
+
+        let report: Vec<u32> = vec![
+            00100,
+            11110,
+            10110,
+            10111,
+            10101,
+            01111,
+            00111,
+            11100,
+            10000,
+            11001,
+            00010,
+            01010,
+        ];
+
+        // Solve
+        let mut values = [0u32; 12];
+        for n in report.iter() {
+            for i in 0..(BIT_WIDTH as usize) {
+                if n & (1 << i) != 0 { // select bit i
+                    values[i] += 1;
+                }
+            }
+        }
+
+        // The threshold for the most common value to be 1
+        // This is 50 % of the # of lines
+        let threshold = (report.len() / 2) as _;
+        let mut gamma: u32 = 0;
+        // Construct gamma
+        for i in 0..(BIT_WIDTH as usize) {
+            if values[i] >= threshold { // select bit i
+                gamma |= 1 << i
+            }
+        }
+
+        let mut epsilon = !gamma; // bitwise not, but only for 12 bits
+        epsilon &= (!0) % (1 << BIT_WIDTH + 1);
+        let p1 = gamma * epsilon;
+
+        // Part 2
+        // o2
+        let mut i = 0;
+        let mut o2 = report.clone();
+        while o2.len() > 1 {
+            // find most common bit
+            let count: u32 = o2.iter().map(|n| (n & (1 << i) != 0) as u32).sum();
+            let bit_criteria = count >= (o2.len()/2) as _;
+            o2 = o2.into_iter().filter(|&n| (n & (1 << i) != 0) == bit_criteria).collect();
+
+            i = (i + 1) % BIT_WIDTH;
+        }
+
+        // co2
+        let mut i = 0;
+        let mut co2 = report.clone();
+        while co2.len() > 1 {
+            // find most common bit
+            let count: u32 = co2.iter().map(|n| (n & (1 << i) != 0) as u32).sum();
+            let bit_criteria = count <= (co2.len()/2) as _;
+            co2 = co2.into_iter().filter(|&n| (n & (1 << i) != 0) == bit_criteria).collect();
+
+            i = (i + 1) % BIT_WIDTH;
+        }
+
+        let pt2 = o2[0] * co2[0];
+        (p1.to_string(), pt2.to_string())
+    }
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -4,12 +4,15 @@ pub trait Day {
 
 pub mod d1;
 pub mod d2;
+pub mod d3;
 
 use d1::Day1;
 use d2::Day2;
+use crate::days::d3::Day3;
 
-pub const DAYS: [fn() -> (String, String); 2] = [
+pub const DAYS: [fn() -> (String, String); 3] = [
     Day1::solve,
     Day2::solve,
+    Day3::solve,
 ];
 
